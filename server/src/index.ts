af6dc95a4b62d011487cc5c5174bf24a2570a31a
ilhamuh97 <ilhamuh97@gmail.com>
2024-02-13 13:59:16 +0100

    add routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use('/', routes);
+app.use('/', routes);
 
 app.get('/', (req, res) => {
   res.status(200).json({
