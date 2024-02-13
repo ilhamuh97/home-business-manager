c982c93528a80e6a42bedcda70158022e221f890
ilhamuh97 <ilhamuh97@gmail.com>
2024-02-13 13:03:37 +0100

    test file

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,12 +31,17 @@ client.on('message', (message) => {
 });
 
 app.use(cors());
-app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', routes);
+// app.use('/', routes);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'hello world',
+  });
+});
 
 /**
  * Error Handler. Provides full stack
