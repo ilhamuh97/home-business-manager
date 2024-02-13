5f2deb91e971a8f63722a2264580ce30340ac370
ilhamuh97 <ilhamuh97@gmail.com>
2024-02-13 12:49:02 +0100

    update files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,43 @@
+'use strict';
+
+import 'dotenv/config';
+import express from 'express';
+import routes from './routes/routes';
+import cors from 'cors';
+import { Client, LocalAuth, NoAuth } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
+import MessageController from './whatsapp-server/Controllers/MessageControllers';
 import errorHandler from 'errorhandler';
-import app from './app';
+
+const app = express();
+
+const client = new Client({
+  authStrategy: new LocalAuth({
+    dataPath: 'mybot',
+  }),
+});
+const messageController = new MessageController(client);
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('WhatsApp Bot is ready!');
+});
+
+client.on('message', (message) => {
+  messageController.handleIncomingMessage(message);
+});
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', routes);
+
 /**
  * Error Handler. Provides full stack
  */
@@ -13,3 +51,7 @@ if (process.env.NODE_ENV === 'development') {
 app.listen(app.get('port'), () => {
   return console.log(`Server is listening on ${app.get('port')}`);
 });
+
+client.initialize();
+
+export default app;
