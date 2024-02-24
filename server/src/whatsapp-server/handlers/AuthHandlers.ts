0aecab0521c335ee1dd3aa8b4635cf125cd2850c
ilhamuh97 <ilhamuh97@gmail.com>
2024-02-24 10:57:13 +0100

    remove comments

--- server/src/whatsapp-server/handlers/AuthHandlers.ts
+++ server/src/whatsapp-server/handlers/AuthHandlers.ts
@@ -8,12 +8,12 @@ import { getRowsObject } from '../../utils/googleSheets';
 
 export async function isAuthenticated(message: WAWebJS.Message) {
   try {
-    const auths = await getAuthPhoneNumbers(); // Fetch authorized phone numbers
-    const phoneNumber = message.author || message.from; // Use message author or sender's number
-    return auths.includes(phoneNumber); // Check if the phone number is authorized
+    const auths = await getAuthPhoneNumbers();
+    const phoneNumber = message.author || message.from;
+    return auths.includes(phoneNumber);
   } catch (error) {
     console.error('Error occurred while checking authentication:', error);
-    return false; // Return false if any error occurs
+    return false;
   }
 }
 
